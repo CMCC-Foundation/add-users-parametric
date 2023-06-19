0f659fd75230d629d2a78f5699c5cb46ceda7ba3
DekraN <marco_chiarelli@yahoo.it>
2023-06-19 14:14:02 +0200

    -MC : added create_new_users_dirs script, to be executed as root from login nodes

---
+++ create_new_users_dirs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+for i in $(tail -n+2 test_user_20230614 | cut -d',' -f1,7);
+do
+	echo "################################################"
+	this_user=$(echo "$i" | cut -d',' -f1)
+	this_div=$(echo "$i" | cut -d',' -f2)
+	echo "This user: ""$this_user"", div: ""$this_div"
+	#su - "$this_user" -c "exit"
+	echo "Back to parent shell."
+	work_dir="/work/""$this_div""/""$this_user"
+	echo "Creating /work dir: ""$work_dir"
+	mkdir -p "$work_dir"
+	echo "Setting UNIX ownerships.."
+	chown "$this_user":"$this_div" "$work_dir"
+	echo "Done."
+	echo "Setting UNIX permissions.."
+	chmod 750 "$work_dir"
+	echo "Done."
+	data_dir="/data/""$this_div""/""$this_user"
+	echo "Creating /data dir: ""$work_dir"
+	mkdir -p "$data_dir"
+	echo "Setting UNIX ownerships.."
+	chown "$this_user":"$this_div" "$data_dir"
+	echo "Done."
+	echo "Setting UNIX permissions.."
+	chmod 700 "$data_dir"
+	echo "Done."
+	echo "################################################"
+done
