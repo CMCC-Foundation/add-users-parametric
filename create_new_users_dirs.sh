b224134f00a8181f6a80239d8fdfe8cbb0d27c93
DekraN <marco_chiarelli@yahoo.it>
2023-06-20 15:03:18 +0200

    -MC : parameterized input file in create_new_users_dirs.sh script, changed ll to ls -lah command

--- create_new_users_dirs.sh
+++ create_new_users_dirs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for i in $(tail -n+2 test_user_20230614 | cut -d',' -f1,7);
+for i in $(tail -n+2 "$1" | cut -d',' -f1,7);
 do
         echo "################################################"
         this_user=$(echo "$i" | cut -d',' -f1)
         this_div=$(echo "$i" | cut -d',' -f2)
         echo "This user: ""$this_user"", div: ""$this_div"
         #su - "$this_user" -c "exit" # decomment this in order to create /users_home/division/user
-        ll "/users_home/""$this_div""/""$this_user"
+        ls -lah "/users_home/""$this_div""/""$this_user"
         echo "Back to parent shell."
         work_dir="/work/""$this_div""/""$this_user"
         echo "Creating /work dir: ""$work_dir"
@@ -17,7 +17,7 @@ do
         echo "Done."
         echo "Setting UNIX permissions.."
         chmod 750 "$work_dir"
-        ll "$work_dir"
+        ls -lah "$work_dir"
         echo "Done."
         data_dir="/data/""$this_div""/""$this_user"
         echo "Creating /data dir: ""$work_dir"
@@ -27,7 +27,7 @@ do
         echo "Done."
         echo "Setting UNIX permissions.."
         chmod 700 "$data_dir"
-        ll "$data_dir"
+        ls -lah "$data_dir"
         echo "Done."
         echo "################################################"
 done
