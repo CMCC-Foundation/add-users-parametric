66b1d72010144e37ba5e9536ad1af894fd392b45
DekraN <marco_chiarelli@yahoo.it>
2023-06-28 10:04:53 +0200

    -MC : enhanced create_new_users_dirs in order to chown to primary group rather than div

--- create_new_users_dirs.sh
+++ create_new_users_dirs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-for i in $(tail -n+2 "$1" | cut -d',' -f1,7);
+for i in $(tail -n+2 "$1" | cut -d',' -f1,6,7);
 do
         echo "################################################"
         this_user=$(echo "$i" | cut -d',' -f1)
-        this_div=$(echo "$i" | cut -d',' -f2)
+        this_grp=$(echo "$i" | cut -d',' -f2)
+        this_div=$(echo "$i" | cut -d',' -f3)
         echo "This user: ""$this_user"", div: ""$this_div"
         #su - "$this_user" -c "exit" # decomment this in order to create /users_home/division/user
         ls -lah "/users_home/""$this_div" | grep "$this_user"
@@ -13,7 +14,7 @@ do
         echo "Creating /work dir: ""$work_dir"
         mkdir -p "$work_dir"
         echo "Setting UNIX ownerships.."
-        chown "$this_user":"$this_div" "$work_dir"
+        chown "$this_user":"$this_grp" "$work_dir"
         echo "Done."
         echo "Setting UNIX permissions.."
         chmod 750 "$work_dir"
@@ -23,7 +24,7 @@ do
         echo "Creating /data dir: ""$work_dir"
         mkdir -p "$data_dir"
         echo "Setting UNIX ownerships.."
-        chown "$this_user":"$this_div" "$data_dir"
+        chown "$this_user":"$this_grp" "$data_dir"
         echo "Done."
         echo "Setting UNIX permissions.."
         chmod 700 "$data_dir"
